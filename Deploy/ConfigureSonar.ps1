2a9379dac2bebc018dfd42df30437a27a4cf9697
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2019-12-15 20:27:19 +0100

    extended sonar powershell modules

--- Deploy/ConfigureSonar.ps1
+++ Deploy/ConfigureSonar.ps1
@@ -20,13 +20,17 @@ function Start-SonarApi{
 }
 
 function Invoke-SonarApiCall{
+    [CmdletBinding()]
     param (      
         [string]
         $ApiUrl,
         [string]
         $Method,
         [Object]
-        $Body
+        $Body,
+        [ValidateRange(0, [Int32]::MaxValue)]
+        [Int]
+        $TimeoutSec = 30
     )
     
     $pair = "$([SonarQubeCommon]::User):$([SonarQubeCommon]::Password)"
@@ -37,15 +41,23 @@ function Invoke-SonarApiCall{
         Authorization = $basicAuthValue
     }
 
-    $arguemtents = @{
-
+    $arguments = @{
+        Uri = "$([SonarQubeCommon]::BaseUrl)/$ApiUrl"
+        Method = $Method
+        Headers = $Headers
+        Body = $Body
+        ErrorVariable = ResponseError
     }
 
-    Invoke-RestMethod -Uri "$([SonarQubeCommon]::BaseUrl)/$ApiUrl" -Method $Method -Headers $Headers -Body $Body
+    if ($TimeoutSec) {
+        $arguments.TimeoutSec = $TimeoutSec
+    }
 
+    Invoke-RestMethod  @arguments
 }
 
 function Set-SonarSetting{
+    [CmdletBinding()]
     param(        
         [string]
         $Key,
@@ -58,37 +70,119 @@ function Set-SonarSetting{
         value = $Value
     }   
 }
-Start-SonarApi -User 'admin' -Password 'admin' -BaseUrl 'https://wa-sonarqubetest.azurewebsites.net'
 
+function Invoke-RestartSonarQube{
+    [CmdletBinding()]
+    param (
+        [string]
+        $WebAppName = "wa-sonarhosting",
+        [ValidateRange(0,[Int32]::MaxValue)]
+        [Int]
+        $MaximumRetryCount = 0,
+        [ValidateRange(1, [Int32]::MaxValue)]
+        [Int]
+        $RetryIntervalSec = 5
+    )
 
-# install azure aad plugin
-$availablePlugin = Invoke-SonarApiCall -ApiUrl "api/plugins/available" -Method Get
-$aadPlugin = $availablePlugin.plugins | Where-Object { $_.key -eq "authaad" }
+    $status = $null
+    $retryCount = 1
 
-Invoke-SonarApiCall -ApiUrl "api/plugins/install" -Method POST -Body @{
-    key = $aadPlugin.key
+    Write-Output "Restarting Sonarqube Server"
+    $webApp = Get-AzWebApp -Name $WebAppName
+    $null = Restart-AzWebApp -ResourceGroupName $webApp.ResourceGroup -Name $webApp.Name
+    #Invoke-SonarApiCall -ApiUrl "api/system/restart" -Method POST
+
+    while ($retryCount -le $MaximumRetryCount) {
+        $retryCount = $retryCount + 1
+        Write-Output "Trying to ping server. Try number $retryCount"
+        $status = Invoke-SonarApiCall -ApiUrl "api/system/status" -Method GET -TimeoutSec 120 -ErrorVariable $ResponseError
+
+        if($status -eq "UP"){            
+            break
+        }else{
+            Write-Output "Server not yet up, retry in $RetryIntervalSec"
+            Start-Sleep -Seconds $RetryIntervalSec
+        }        
+    }
 }
 
-#Restart-AzWebApp -ResourceGroupName "Default-Web-WestUS" -Name "ContosoSite"
-#Invoke-SonarApiCall -ApiUrl "api/system/restart"  -Method POST
+function Install-SonarQubePlugin{
+    [CmdletBinding()]
+    param (
+        [string]
+        $Name
+    )
 
-$status = $null
-while ($status -ne "pong") {
-    $status = Invoke-SonarApiCall -ApiUrl "api/system/status" -Method GET
-    Start-Sleep -Seconds 10
+    $installedPlugins = Invoke-SonarApiCall -ApiUrl "api/plugins/installed" -Method Get
+    $isInstalled = $null -ne ($installedPlugins.plugins | Where-Object { $_.key -eq $Name })
+
+    if($isInstalled){
+
+        Write-Warning "Plugin $Name is already installed"
+        return $false
+
+    }else{
+
+        $availablePlugin = Invoke-SonarApiCall -ApiUrl "api/plugins/available" -Method Get
+        $aadPlugin = $availablePlugin.plugins | Where-Object { $_.key -eq $Name }
+
+        Invoke-SonarApiCall -ApiUrl "api/plugins/install" -Method POST -Body @{
+            key = $aadPlugin.key
+        }
+    }
+
+    return $true
 }
 
-#only allow logged in users to see something
-Set-SonarSetting -Key "sonar.forceAuthentication" -Value "true"
+function Initialize-SonarQubeConfiguration{
+    [CmdletBinding(DefaultParameterSetName='IntegratedLogin')]
+    param (
+        [string]
+        [Parameter(ParameterSetName='AzureADLogin')]
+        [Parameter(ParameterSetName='IntegratedLogin')]
+        $WebAppName = "wa-sonarhosting",
+        [string]
+        [Parameter(ParameterSetName='IntegratedLogin')]
+        [Parameter(ParameterSetName='AzureADLogin')]
+        $AdminUser = 'admin',
+        [string]
+        [Parameter(ParameterSetName='IntegratedLogin')]
+        [Parameter(ParameterSetName='AzureADLogin')]
+        $AdminPassword = 'admin',
+        [Parameter(Mandatory=$true,ParameterSetName='AzureADLogin')]
+        [string]
+        $AadTenantId,
+        [Parameter(Mandatory=$true,ParameterSetName='AzureADLogin')]
+        [string]
+        $AadClientId,
+        [Parameter(Mandatory=$true,ParameterSetName='AzureADLogin')]
+        [string]
+        $AadClientSecret
+    )
 
-#set settings for aad plugin
-Set-SonarSetting -Key "sonar.auth.aad.tenantId" -Value $AadTenantId
-Set-SonarSetting -Key "sonar.auth.aad.clientId.secured" -Value $AadClientId
-Set-SonarSetting -Key "sonar.auth.aad.clientSecret.secured" -Value $AadClientSecret
-Set-SonarSetting -Key "sonar.auth.aad.enableGroupsSync" -Value "true"
-Set-SonarSetting -Key "sonar.auth.aad.loginStrategy" -Value "Same as Azure AD login"
-Set-SonarSetting -Key "sonar.auth.aad.enabled" -Value "true"
+    $SonarBaseUrl = "https://$WebAppName.azurewebsites.net"
+
+    Start-SonarApi -User $AdminUser -Password $AdminPassword -BaseUrl $SonarBaseUrl
 
 
-#$definitions = Invoke-SonarApiCall -ApiUrl "api/settings/list_definitions" -Method GET
+    #only allow logged in users to see something
+    Set-SonarSetting -Key "sonar.forceAuthentication" -Value "true"
 
+    #set settings for aad plugin
+    if ($PsCmdlet.ParameterSetName -eq "AzureADLogin") {
+
+        # install azure aad plugin
+        $needsRestart = Install-SonarQubePlugin -Name "authaad"
+        if($needsRestart ){
+            Invoke-RestartSonarQube -MaximumRetryCount 0 -RetryIntervalSec 30 -WebAppName $WebAppName
+        }
+
+        Set-SonarSetting -Key "sonar.auth.aad.tenantId" -Value $AadTenantId
+        Set-SonarSetting -Key "sonar.auth.aad.clientId.secured" -Value $AadClientId
+        Set-SonarSetting -Key "sonar.auth.aad.clientSecret.secured" -Value $AadClientSecret
+        Set-SonarSetting -Key "sonar.auth.aad.enableGroupsSync" -Value "true"
+        Set-SonarSetting -Key "sonar.auth.aad.loginStrategy" -Value "Same as Azure AD login"
+        #Set-SonarSetting -Key "sonar.auth.aad.enabled" -Value "true"
+
+    }
+}
