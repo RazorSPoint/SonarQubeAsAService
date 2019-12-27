f02dcfc439fc2e096aa449f3f09f5dd63b8e03ff
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2019-12-27 18:57:00 +0100

    Delete TestCalls.ps1

--- TestCalls.ps1
+++
@@ -1,7 +0,0 @@
-& '.Deploy\RunPowerShellKudu.ps1' `
-    -WebsiteName "mykuduest" `
-    -SqlServerName "sqlserv-sonarqubetest" `
-    -SqlDatabase "sqldb-sonarqubetest"`
-    -SqlDatabaseAdmin "sonaradmin"`
-    -SqlDatabaseAdminPassword "sonar2019!"`
-    -ResourceGroupName "RG_SonarQubeTest02_DEV"
